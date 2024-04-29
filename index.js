a78bb28f424f42bd66bb69f7b786badaab52e4e6
ahosanul81 <ahosanul81@gmail.com>
2024-04-29 20:47:28 +0600

    connect to mongodb

--- index.js
+++ index.js
@@ -27,8 +27,6 @@ const client = new MongoClient(uri, {
 });
 
 
-
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -44,8 +42,6 @@ async function run() {
       res.send(result)
     })
 
-
-    
     // read by specific email
     app.get('/craft_items', async (req, res) => {
       const email = req.query.email;
